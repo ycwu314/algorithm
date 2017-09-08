6484b0b787ed4f4f8ca962a93f14daea833dfbc9
ycwu314 <535215779@qq.com>
2017-09-08 16:44:38 +0800

    栈：两个栈排序

--- src/main/java/com/example/stack/StackMisc.java
+++ src/main/java/com/example/stack/StackMisc.java
@@ -3,8 +3,11 @@ package com.example.stack;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Stack;
+import java.util.stream.IntStream;
 
 /**
  * Created by Administrator on 2017/9/7.
@@ -107,5 +110,58 @@ public class StackMisc {
         int[] a = reverseStackRecursively(new int[]{1, 2, 3, 4, 5}, 5);
         System.out.println(Arrays.toString(a));
         Assert.assertArrayEquals(new int[]{5, 4, 3, 2, 1}, a);
+    }
+
+    /////////////////
+
+
+    /**
+     * 请编写一个程序，按升序对栈进行排序（即最大元素位于栈顶），要求最多只能使用一个额外的栈存放临时数据，但不得将元素复制到别的数据结构中。
+     * <p>
+     * ps: 只能使用一个额外的栈
+     *
+     * @param numbers representing a stack
+     * @return
+     */
+    public ArrayList<Integer> twoStacksSort(int[] numbers) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (numbers == null || numbers.length == 0) {
+            return result;
+        }
+
+        for (int i = numbers.length - 1; i >= 0; i--) {
+            int curr = numbers[i];
+
+            int k = i;
+            while (!result.isEmpty()) {
+                // peek
+                int topOfResultStack = result.get(result.size() - 1);
+
+                if (curr > topOfResultStack) {
+                    // put element of result stack into numbers stack to avoid using extra space
+                    numbers[k++] = topOfResultStack;
+                    result.remove(result.size() - 1);
+                } else {
+                    break;
+                }
+            }
+
+            numbers[k] = curr;
+            // push elements which come from result stack back into result stack
+            while (k >= i) {
+                result.add(numbers[k--]);
+            }
+        }
+
+        return result;
+    }
+
+
+    @Test
+    public void testTwoStacksSort() throws Exception {
+        ArrayList<Integer> a = twoStacksSort(new int[]{1, 7, 8, 4, 5});
+        int[] ra = a.stream().mapToInt(i -> i).toArray();
+        Assert.assertArrayEquals(new int[]{8, 7, 5, 4, 1}, ra);
+
     }
 }
